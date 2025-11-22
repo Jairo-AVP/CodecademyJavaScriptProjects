a057877abba376beea185e73827b4a994e017909
jairovera88@outlook.com <jairovera88@outlook.com>
2025-11-22 13:09:45 -0300

    Fixed a typo

---
+++ magicEightBall.js
@@ -0,0 +1,41 @@
+/* 
+Magic Eight Ball
+
+You’ve learned a powerful tool in JavaScript: control flow! It’s so powerful, in fact, that it can be used to tell someone’s fortune.
+
+In this project, we will build a Magic Eight Ball using control flow in JavaScript.
+
+The user will be able to input a question, and then our program will output a random fortune.
+*/
+
+let userName = 'Pamela';
+userName ? console.log(`Hello, ${userName}!`): console.log('Hello!');
+
+let userQuestion = 'Will I win the lottery?';
+console.log(`${userName} asks: ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+// console.log(randomNumber);
+
+// Determine the magic Eight Ball's response based on the random number
+let eightBall = randomNumber;
+
+if (randomNumber === 0) {
+  eightBall = 'It is certain'
+} else if (randomNumber === 1) {
+  eightBall = 'It is decidedly so';
+} else if (randomNumber === 2) {
+  eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3) {
+  eightBall = 'Cannot predict now';
+} else if (randomNumber === 4) {
+  eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+  eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+  eightBall = 'Outlook not so good';
+} else if (randomNumber === 7) {
+  eightBall = 'Signs point to yes';
+}
+
+console.log(eightB

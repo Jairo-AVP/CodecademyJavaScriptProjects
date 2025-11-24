b217ab4f137a20843b162b2e22152fb6c316f3a5
jairovera88@outlook.com <jairovera88@outlook.com>
2025-11-24 20:05:02 -0300

    Implement switch-case logic for program flow control

--- magicEightBall.js
+++ magicEightBall.js
@@ -8,34 +8,89 @@ In this project, we will build a Magic Eight Ball using control flow in JavaScri
 The user will be able to input a question, and then our program will output a random fortune.
 */
 
-let userName = 'Pamela';
-userName ? console.log(`Hello, ${userName}!`): console.log('Hello!');
+// let userName = 'Pamela';
+// userName ? console.log(`Hello, ${userName}!`): console.log('Hello!');
 
-let userQuestion = 'Will I win the lottery?';
-console.log(`${userName} asks: ${userQuestion}`);
+// let userQuestion = 'Will I win the lottery?';
+// console.log(`${userName} asks: ${userQuestion}`);
 
+// let randomNumber = Math.floor(Math.random() * 8);
+// // console.log(randomNumber);
+
+// // Determine the magic Eight Ball's response based on the random number
+// let eightBall = randomNumber;
+
+// if (randomNumber === 0) {
+//   eightBall = 'It is certain'
+// } else if (randomNumber === 1) {
+//   eightBall = 'It is decidedly so';
+// } else if (randomNumber === 2) {
+//   eightBall = 'Reply hazy try again';
+// } else if (randomNumber === 3) {
+//   eightBall = 'Cannot predict now';
+// } else if (randomNumber === 4) {
+//   eightBall = 'Do not count on it';
+// } else if (randomNumber === 5) {
+//   eightBall = 'My sources say no';
+// } else if (randomNumber === 6) {
+//   eightBall = 'Outlook not so good';
+// } else if (randomNumber === 7) {
+//   eightBall = 'Signs point to yes';
+// }
+
+// console.log(eightBall)
+
+
+/*
+Final Task: If you started with if / else if / else statements, convert them to a switch statements.
+*/
+// Getting the name of the person who will be told their fortune.
+let userName = 'Cutucho'
+
+// Creating ternary expression that decides what to do if the user enters a name or not.
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+// Getting the question that will be answered by the Magic Eight Ball.
+let userQuestion = 'Will I win the lottery?'
+// console.log(`${userName} would like to know: `)
+// console.log(userQuestion)
+console.log(`${userName ? userName : 'User'} would like to know: ${userQuestion}`);
+
+// Generate a random number between 0 and 7 for the Magic Eight Ball response
 let randomNumber = Math.floor(Math.random() * 8);
-// console.log(randomNumber);
 
-// Determine the magic Eight Ball's response based on the random number
-let eightBall = randomNumber;
+let eightBall = '';
 
-if (randomNumber === 0) {
-  eightBall = 'It is certain'
-} else if (randomNumber === 1) {
+// Creating a control flow for answering telling the fortune.
+switch (randomNumber) {
+  case 0:
+  eightBall = 'It is certain';
+  break;
+  case 1:
   eightBall = 'It is decidedly so';
-} else if (randomNumber === 2) {
+  break;
+  case 2:
   eightBall = 'Reply hazy try again';
-} else if (randomNumber === 3) {
+  break;
+  case 3:
   eightBall = 'Cannot predict now';
-} else if (randomNumber === 4) {
+  break;
+  case 4:
   eightBall = 'Do not count on it';
-} else if (randomNumber === 5) {
+  break;
+  case 5:
   eightBall = 'My sources say no';
-} else if (randomNumber === 6) {
+  break;
+  case 6:
   eightBall = 'Outlook not so good';
-} else if (randomNumber === 7) {
+  break;
+  case 7:
   eightBall = 'Signs point to yes';
+  break;
+  default:
+  eightBall = 'It is certain';
+  break;
 }
 
-console.log(eightBall)
+// Logging to the terminal the Magic Eight's Answer.
+console.log(eightBall);
